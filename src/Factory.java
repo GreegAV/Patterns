65229c54f98ad98c505a95f6e8b85a8d0a2fe314
GreegAV <greegav@gmail.com>
2017-12-17 18:45:08 +0200

    Initial commit

---
+++ src/Factory.java
@@ -0,0 +1,38 @@
+/**
+ * Created by avg-m on 17/12/2017.
+ */
+public class Factory {
+    public static void main(String[] args) {
+        MyFactory myFactory = new MyFactory();
+        myFactory.create("bmw").drive();
+        myFactory.create("toyota").drive();
+    }
+}
+
+interface Car{
+    void drive();
+}
+
+class Toyota implements Car{
+    @Override
+    public void drive() {
+        System.out.println("Drive your dream! Toyota.");
+    }
+}
+
+class BMW implements Car{
+    @Override
+    public void drive() {
+        System.out.println("The power of BMW!");
+    }
+}
+
+class MyFactory {
+    public Car create(String carType){
+        switch (carType){
+            case "bmw": return new BMW();
+            case "toyota": return new Toyota();
+            default: return null;
+        }
+    }
+}
